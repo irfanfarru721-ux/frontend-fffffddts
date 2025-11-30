a13f521a276e2e04585024825bf9f348f9fcfd5c
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 14:53:32 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import ModulesPage from "./pages/ModulesPage.jsx";
+import VendorsPage from "./pages/VendorsPage.jsx";
+import ProductsPage from "./pages/ProductsPage.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
-import Home from "./pages/Home.jsx"; // optional, redirect after login/signup
+import Cart from "./pages/Cart.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import Orders from "./pages/Orders.jsx";
 
-export default function App() {
+export default function App(){
   return (
-    <BrowserRouter>
+    <>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/modules' element={<ModulesPage/>} />
+        <Route path='/modules/:moduleId/vendors' element={<VendorsPage/>} />
+        <Route path='/vendor/:vendorId/products' element={<ProductsPage/>} />
+        <Route path='/product/:id' element={<ProductDetails/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/checkout' element={<Checkout/>} />
+        <Route path='/orders' element={<Orders/>} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
