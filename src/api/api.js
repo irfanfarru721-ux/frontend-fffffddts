afe3a6cf763464a7816c1b4cc78dbb5a12e4c3cd
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 14:45:21 +0530

    Add files via upload

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
+export const API_BASE = "https://srudentbackend-1.onrender.com/api";
 
-const instance = axios.create({
-baseURL: process.env.REACT_APP_API_BASE || '/api',
-withCredentials: true,
+const axiosInstance = axios.create({
+  baseURL: API_BASE,
+  headers: { "Content-Type": "application/json" },
 });
 
-
-// attach token from localStorage (or from AuthContext global store)
-instance.interceptors.request.use((config) => {
-const token = localStorage.getItem('token');
-if (token) config.headers.Authorization = `Bearer ${token}`;
-return config;
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
+// Auth
+export const loginUser = (data) => axiosInstance.post("/auth/login", data);
+export const registerUser = (data) => axiosInstance.post("/auth/register", data);
 
-export default instance;
+// Modules / Categories
+export const getModules = () => axiosInstance.get("/modules");
+
+// Vendors / Shops
+export const getVendorsByModule = (moduleId) => axiosInstance.get(`/vendors/module/${moduleId}`);
+
+// Products
+export const getProductsByVendor = (vendorId) => axiosInstance.get(`/products/vendor/${vendorId}`);
+export const getProduct = (id) => axiosInstance.get(`/products/${id}`);
+export const getAllProducts = () => axiosInstance.get("/products");
